69be5dacb5a544cacaa49e74755e6e03cd92405a
ishanverma41 <ishanverma041@gmail.com>
2023-12-01 17:02:42 +0530

    Assessment 3 changes

---
+++ app/(products)/products/page.jsx
@@ -0,0 +1,27 @@
+import Card from "@/components/card/Card";
+import PageHeader from "@/components/header/PageHeader";
+import { getVideos } from "@/lib/firebase/getVideos";
+
+async function VideosPage() {
+    const payload = await getVideos();
+
+
+
+
+    return (
+        <>
+            <PageHeader title="Video Vault: Explore Our Collection" tagline="Uncover, Watch, and Experience the Stories That Matter." />
+            <div className="container" >
+                <div className="row" >
+                    {
+                        payload && payload.map(item =>
+                            <div key={item.video_id} className="col col-md-3 mt-2">
+                                <Card videoData={item} /></div>)
+                    }
+                </div>
+            </div >
+        </>
+    )
+}
+
+export default VideosPage
