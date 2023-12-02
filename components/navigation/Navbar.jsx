d78e04f17eab795323cff8701e7ac160e619d1e1
ishanverma41 <ishanverma041@gmail.com>
2023-12-02 09:06:52 +0530

    Update Navbar.jsx

--- components/navigation/Navbar.jsx
+++ components/navigation/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar(){
           <label className="toggle">Explore</label>
           <input type="checkbox" id="drop"/>
           <ul className="menu mt-2">  
-             <li><a href="/many-items" className="mt-1">Videos</a></li>       
+             <li><a href="/products" className="mt-1">Videos</a></li>       
             <li><a href="https://github.com/ishanverma41/dmit2008-assignment2-ishanverma"><img src="/images/github.png"></img></a></li>
           </ul>
         </nav>
